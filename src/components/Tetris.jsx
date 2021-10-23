19f4a43dd70f5caed22dadd3a3c27ec8b71a77c2
Kimiko <kimikoescovilla@gmail.com>
2021-10-23 21:35:52 +0900

    feat: added states.

--- src/components/Tetris.jsx
+++ src/components/Tetris.jsx
@@ -13,20 +13,28 @@ import Display from './Display';
 import StartButton from './StartButton';
 
 function Tetris() {
+    const [dropTime, setDropTime] = useState(null);
+    const [gameOver, setGameOver] = useState(false);
 
+    const [player] = usePlayer();
+    const [stage, setStage] = useStage(player);
 
     console.log('re-render');
 
     return (
         <StyledTetrisWrapper>
             <StyledTetris>
-                <Stage stage={createStage()}/>
+                <Stage stage={stage}/>
                 <aside>
+                    { gameOver ? (
+                        <Display gameOver={gameOver} text="Game Over"/>
+                    ) : (
                     <div>
                         <Display text="Score"/>
                         <Display text="Rows"/>
                         <Display text="Level"/>
                     </div>
+                    )}
                     <StartButton/>
                 </aside>
             </StyledTetris>
