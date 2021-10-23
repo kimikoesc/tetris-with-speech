bffd508ef286bc192d2beaa02a55a6e6bd3dee70
Kimiko <kimikoescovilla@gmail.com>
2021-10-23 20:24:49 +0900

    feat: added cells and its styling.

--- src/components/Cell.jsx
+++ src/components/Cell.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import React from 'react';
+import { StyledCell } from'./styles/style-cell';
+import { allTetrominoes } from '../tetrominos';
 
 function Cell({ type }) {
     return (
-        <div>
+        <StyledCell type={'L'} color={ allTetrominoes['L'].color }>
             Cell
-        </div>
+        </StyledCell>
     )
 }
 
---
+++ src/tetrominos.js
@@ -0,0 +1,68 @@
+export const allTetrominoes = {
+    0: { shape: [[0]], color: '0, 0, 0'},
+    I: { 
+        shape: [
+            [0, "I", 0, 0],
+            [0, "I", 0, 0],
+            [0, "I", 0, 0],
+            [0, "I", 0, 0],
+        ], 
+        color: '80, 227, 230'
+    },
+    J: { 
+        shape: [
+            [0, "J", 0],
+            [0, "J", 0],
+            [0, "J", 0],
+            ["J", "J", 0],
+        ], 
+        color: '36, 95, 223'
+    },
+    L: { 
+        shape: [
+            [0, "L", 0],
+            [0, "L", 0],
+            [0, "L", 0],
+            [0, "L", "L"],
+        ], 
+        color: '223, 173, 36'
+    },
+    O: { 
+        shape: [
+            ["O", "O"],
+            ["O", "O"]
+        ], 
+        color: '223, 217, 36'
+    },
+    L: { 
+        shape: [
+            [0, "S", "S"],
+            ["S", "S", 0],
+            [0, 0, 0]
+        ], 
+        color: '41, 211, 56'
+    },
+    Z: { 
+        shape: [
+            ["Z", "Z", 0],
+            [0, "Z", "Z"],
+            [0, 0, 0]
+        ], 
+        color: '41, 45, 56'
+    },
+    T: { 
+        shape: [
+            [0, 0, 0],
+            ['T', 'T', 'T'],
+            [0, 'T', 0]
+        ], 
+        color: '132, 61, 198'
+    }
+};
+
+export const randomTetromino = () => {
+    const tetrominoes = 'IJLOSTZ';
+    const randomTetromino = 
+    tetrominoes[Math.floor(Math.random() * tetrominoes.length)];
+    return allTetrominoes[randomTetromino];
+}
